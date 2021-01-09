0ba44ca4fecac870800f5993877c0714f9b4186c
sandeeptechnical <sandeepchatterjee8808@gmail.com>
2021-01-09 16:53:04 +0530

    publishing my firstwebsite
    wow, i'm happy

---
+++ new.js
@@ -0,0 +1,34 @@
+
+alert('hello');
+/*var a=10;
+var b=9;
+var c=a+b;
+var d=c++;
+var e=a==b;
+var f=a*c;
+var h=a/b;
+document.write(c);
+document.write(e);
+document.write("<br>");
+document.write(d);
+document.write(f);
+document.write(h);
+document.write("<br>");
+if(a<b)
+{ document.write("yo man its true");
+}
+else
+{ document.write("yo man its false");
+}
+switch(a)
+{case 10: document.write("its a 10 no. case");
+break;
+case 10: document.write("its a 10 no. case");
+break;
+}*/
+var count=1;
+while(count<10)
+{
+	document.write("sandeep technical <br></br>");
+	count++;
+}
